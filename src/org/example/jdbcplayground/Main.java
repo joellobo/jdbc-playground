6921b9f9e5a60e358f6dc085858d07e2af6bedd5
64943127304 <joellobo@gmail.com>
2019-02-13 12:56:42 -0300

    add project

---
+++ src/org/example/jdbcplayground/Main.java
@@ -0,0 +1,8 @@
+package org.example.jdbcplayground;
+
+public class Main {
+
+	public static void main(String[] args) {
+		MyConnection con = new MyConnection();
+	}
+}
---
+++ src/org/example/jdbcplayground/MyConnection.java
@@ -0,0 +1,11 @@
+package org.example.jdbcplayground;
+
+import java.sql.Connection;
+
+public class MyConnection {
+	
+	private Connection connection;
+	
+	public MyConnection() {
+	}
+}
